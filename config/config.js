6fefdc4dd8bb68b6b8aac337cfb1ecdc332fe2c2
franco-pajello <francoagustinpajello@gmail.com>
2023-03-24 23:21:54 -0300

    cambiamos en npm run dw

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ require('dotenv').config({
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT || 8080,
-  TIPO_PERSISTENCIA: process.env.TIPO_PERSISTENCIA || 'mongo',
-  EMAILADMIN: process.env.EMAILADMIN || 'francopajello@gmail.com',
-  PASSEMAILADMIN: process.env.PASSEMAILADMIN || 'dsycrdenpdrtlzpn',
-  HOST: process.env.HOST || 'mongodb+srv://franco-pajello:tJ7LmKYYZwptLS9G@cluster0.8t2nx9j.mongodb.net/ecommerce',
-  LOCALHOST: process.env.LOCALHOST || '127.0.0.1',
+  PORT: process.env.PORT ,
+  TIPO_PERSISTENCIA: process.env.TIPO_PERSISTENCIA ,
+  EMAILADMIN: process.env.EMAILADMIN ,
+  PASSEMAILADMIN: process.env.PASSEMAILADMIN,
+  HOST: process.env.HOST ,
+  LOCALHOST: process.env.LOCALHOST,
 };
